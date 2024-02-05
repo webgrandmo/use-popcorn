645702354d954f83bc8fc39d65a57aedfc8fad23
Andriy Trofimov <andriy.trofimov@valtech.com>
2024-02-05 18:08:52 +0200

    start component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './StarRating';
+
+
+function Test()
+{
+	const [movieRating, setMovieRating] = useState(0)
+	return <div>
+		<StarRating maxRating={10} color='blue' onSetRating={setMovieRating} />
+		<p>Movie has rating {movieRating} </p>
+	</div>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+		<StarRating maxRating={10} color='red' size={24} className="test" />
+		<StarRating defaultRating={3} />
+		<Test />
 	</React.StrictMode>
 );
